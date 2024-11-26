a0441a56a8a9a2188385c37e7106f6473b8bdd49
Liza Shulika <liza.shulika2009@gmail.com>
2024-11-26 17:26:07 +0200

    vector2

--- app/main.py
+++ app/main.py
@@ -2,21 +2,21 @@ import math
 
 
 class Vector:
-    def __init__(self, x: float, y: float) -> None:
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(self, x_cord: float, y_cord: float) -> None:
+        self.x_cord = round(x_cord, 2)
+        self.y_cord = round(y_cord, 2)
 
     def __add__(self, other: "Vector") -> "Vector":
-        return Vector(self.x + other.x, self.y + other.y)
+        return Vector(self.x_cord + other.x_cord, self.y_cord + other.y_cord)
 
     def __sub__(self, other: "Vector") -> "Vector":
-        return Vector(self.x - other.x, self.y - other.y)
+        return Vector(self.x_cord - other.x_cord, self.y_cord - other.y_cord)
 
     def __mul__(self, other: "Vector") -> any:
         if isinstance(other, (int, float)):
-            return Vector(self.x * other, self.y * other)
+            return Vector(self.x_cord * other, self.y_cord * other)
         elif isinstance(other, Vector):
-            return self.x * other.x + self.y * other.y
+            return self.x_cord * other.x_cord + self.y_cord * other.y_cord
         else:
             raise TypeError(
                 "Unsupported operand type(s)"
@@ -33,13 +33,13 @@ class Vector:
                    end_point[1] - start_point[1])
 
     def get_length(self) -> float:
-        return math.sqrt(self.x ** 2 + self.y ** 2)
+        return math.sqrt(self.x_cord ** 2 + self.y_cord ** 2)
 
     def get_normalized(self) -> "Vector":
         length = self.get_length()
         if length == 0:
             raise ValueError("Cannot normalize a zero-length vector.")
-        return Vector(self.x / length, self.y / length)
+        return Vector(self.x_cord / length, self.y_cord / length)
 
     def angle_between(self, other: "Vector") -> int:
         dot_product = self * other
@@ -57,8 +57,8 @@ class Vector:
 
     def rotate(self, degrees: int) -> "Vector":
         radians = math.radians(degrees)
-        new_x = (self.x * math.cos(radians)
-                 - self.y * math.sin(radians))
-        new_y = (self.x * math.sin(radians)
-                 + self.y * math.cos(radians))
+        new_x = (self.x_cord * math.cos(radians)
+                 - self.y_cord * math.sin(radians))
+        new_y = (self.x_cord * math.sin(radians)
+                 + self.y_cord * math.cos(radians))
         return Vector(new_x, new_y)
